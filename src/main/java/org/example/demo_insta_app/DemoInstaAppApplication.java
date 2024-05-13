0417a0c26349b2deb786d9a927102636a24f4a15
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-05-13 17:39:29 +0500

    Initial commit

---
+++ src/main/java/org/example/demo_insta_app/DemoInstaAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.demo_insta_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInstaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoInstaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demo_insta_app/DemoInstaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo_insta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInstaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
